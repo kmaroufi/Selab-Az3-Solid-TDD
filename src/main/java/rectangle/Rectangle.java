c8a684d0dd867c10f79b4ffb1d3a75f7dc4fa121
kmaroufi <kmaroufi76@gmail.com>
2021-11-01 17:42:30 +0330

    create Rectangle class

---
+++ src/main/java/rectangle/Rectangle.java
@@ -0,0 +1,11 @@
+package rectangle;
+
+public class Rectangle {
+    public float width;
+    public float height;
+
+    public Rectangle(float width, float height) {
+        this.width = width;
+        this.height = height;
+    }
+}
