e77233d1e1f4036174295acd11e5cce5d7020ac3
vsaeide <vsaeide7798@gmail.com>
2021-11-02 15:56:04 +0330

    Square setter and getter test  added

--- src/test/java/Shapes/SquareTest.java
+++ src/test/java/Shapes/SquareTest.java
@@ -10,7 +10,20 @@ public class SquareTest {
     Square square = new Square(3);
 
     @Test
-    public void getArea() {
+    public void computeArea() {
         assertEquals(9,square.computeArea());
     }
+
+
+    @Test
+    public void setSide() {
+        square.setSide(4);
+        assertEquals(4,square.getSide());
+    }
+
+    @Test
+    public void getSide() {
+        assertEquals(3,square.getSide());
+    }
+
 }
