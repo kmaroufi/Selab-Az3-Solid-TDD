553188731bb7356e5b42df4853f56c600673950d
vsaeide <vsaeide7798@gmail.com>
2021-11-02 17:58:18 +0330

    square illegal input handled

--- src/main/java/Shapes/Square.java
+++ src/main/java/Shapes/Square.java
@@ -5,19 +5,25 @@ public class Square extends Shape {
     private float side;
 
     public Square (float side) {
+        if (side <= 0) throw new IllegalArgumentException();
         this.side = side;
     }
-    public float getSide() {
-        return side;
-    }
-
-    public void setSide(float side) {
-        this.side = side;
-    }
-
 
     @Override
     float computeArea() {
         return side*side;
     }
+
+
+    public float getSide() {
+        return side;
+    }
+
+    public void setSide(float side) {
+        if (side <= 0) throw new IllegalArgumentException();
+        this.side = side;
+    }
+
+
+
 }
